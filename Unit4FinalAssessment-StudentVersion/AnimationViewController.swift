c190f6dc246f5a229570593273cc868238a922cb
Ashlee Krammer <ashlee.alyse@me.com>
2018-01-12 11:53:13 -0500

    Adding Picker View

--- Unit4FinalAssessment-StudentVersion/AnimationViewController.swift
+++ Unit4FinalAssessment-StudentVersion/AnimationViewController.swift
@@ -10,15 +10,29 @@ import UIKit
 
 class AnimationViewController: UIViewController {
     
+    var pauseOrPlay = "pause"
+    
+    //Snowman Image View
     lazy var snowmanImage: UIImageView = {
         let image = UIImageView()
         image.image = #imageLiteral(resourceName: "snowman")
         return image
     }()
     
+    //Picker View
     lazy var picker: UIPickerView = {
        let picker = UIPickerView()
         return picker
+    }()
+    
+    //Play Pause Button
+    lazy var playPauseButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .blue
+        button.titleLabel?.text = "play"
+        button.layer.cornerRadius = button.bounds.width / 2.0
+        button.layer.masksToBounds = true
+        return button
     }()
     
     override func viewDidLoad() {
@@ -26,13 +40,17 @@ class AnimationViewController: UIViewController {
         self.view.backgroundColor = UIColor(red: 0.6, green: 0.6, blue: 0.9, alpha: 1.0)
         view.addSubview(snowmanImage)
         view.addSubview(picker)
+        view.addSubview(playPauseButton)
         setUpImage()
+        setUpButton()
         setUpPicker()
+        self.picker.dataSource = self
+        self.picker.delegate = self
     }
     
     
-    //Constraints
     
+    //Constraints
     func setUpImage() {
         snowmanImage.translatesAutoresizingMaskIntoConstraints = false
         snowmanImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16).isActive = true
@@ -47,8 +65,40 @@ class AnimationViewController: UIViewController {
         picker.topAnchor.constraint(equalTo: snowmanImage.bottomAnchor, constant: 8).isActive = true
         picker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         picker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        picker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        picker.bottomAnchor.constraint(equalTo: playPauseButton.topAnchor).isActive = true
     }
+    
+    func setUpButton() {
+        playPauseButton.translatesAutoresizingMaskIntoConstraints = false
+        playPauseButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -175).isActive = true
+            playPauseButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 175).isActive = true
+        playPauseButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8).isActive = true
+        playPauseButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1)
+    }
+    
+}
+
+extension AnimationViewController: UIPickerViewDelegate {
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Default"
+    }
+    
+}
+
+extension AnimationViewController: UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+   
+        
+        
+        return 2
+    }
+    
     
     
 }
